6cb8e57a96f577a47fc5a10516c69e947265ad28
Eduardo-Conegundes <duduconegundes@gmail.com>
2021-04-06 21:00:51 -0300

    classes implementadas

---
+++ src/Filosofo.java
@@ -0,0 +1,40 @@
+package src;
+
+/*
+Cinco filósofos estão sentados em uma mesa redonda para jantar. 
+Cada filósofo tem um prato com espaguete à sua frente. 
+Cada prato possui um garfo para pegar o espaguete. 
+O espaguete está muito escorregadio e, para que um filósofo consiga comer, 
+será necessário utilizar dois garfos. 
+
+Lembre-se que é apenas uma analogia. Nesse sentido, cada filósofo alterna entre duas tarefas: 
+comer ou pensar. Quando um filósofo fica com fome, ele tenta pegar os garfos à sua esquerda e 
+à sua direita; um de cada vez, independente da ordem. Caso ele consiga pegar dois garfos, 
+ele come durante um determinado tempo e depois recoloca os garfos na mesa. Em seguida ele volta a pensar.
+*/
+
+public class Filosofo extends Thread{
+
+    private int id;
+
+    public void Filosofo(){
+      
+    }
+
+    
+    public void run() {
+        
+    }
+
+    void comer(){
+
+    }
+
+    void pensar(){
+
+        
+    }
+
+
+
+}
---
+++ src/Garfo.java
@@ -0,0 +1,8 @@
+package src;
+import java.util.ArrayList;
+
+public class Garfo{
+
+  ArrayList<Integer> garfo = new ArrayList<Integer>();
+
+}
